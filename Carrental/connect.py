5d00a7bedb4b76e92903d4f1946b7b5a699d08c4
dorachenxiW <125512384+dorachenxiW@users.noreply.github.com>
2023-07-27 09:08:45 +1200

    Delete Carrental directory

--- Carrental/connect.py
+++
@@ -1,7 +0,0 @@
-# These details are available on the first MySQL Workbench screen
-# Usually called 'Local Instance'
-dbuser = "root" #PUT YOUR MySQL username here - usually root
-dbpass = "zxcqw098" #PUT YOUR PASSWORD HERE
-dbhost = "localhost" 
-dbport = "3306"
-dbname = "carrental"
