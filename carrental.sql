ac42a7ae2082e9597311a45eb98575fed8ae9889
dorachenxiW <Dora.Wang@lincolnuni.ac.nz>
2023-07-27 14:51:03 +1200

    database change

--- carrental.sql
+++ carrental.sql
@@ -1,49 +1,51 @@
 create database carrental;
 use carrental;
 
-create table catergory (
-ID INT NOT NULL,
-name varchar(100),
-primary key(ID)
-);
-
-create table users (
-userID INT NOT NULL auto_increment,
+create table user (
+userid INT NOT NULL auto_increment,
 username varchar(255) NOT NULL,
 firstname varchar(100),
 lastname varchar(100),
 password varchar(255) NOT NULL,
 email varchar (100) NOT NULL,
-catergoryID INT,
-primary key (userID),
-foreign key (catergoryID) references catergory (ID)
+catergory varchar(60),
+primary key (userid)
 );
 
-create table cars (
-carID INT NOT NULL auto_increment,
+create table car (
+carid INT NOT NULL auto_increment,
 numberplate varchar(100),
 model varchar(255),
-userID INT,
+colour varchar(60),
+year int,
 status varchar(100),
 rentalfee decimal(8,2),
-primary key (carID),
-foreign key (userID) references users(userID)
+primary key (carid)
 );
 
-insert into catergory values
-(1, 'customers'),
-(2, 'staff'),
-(3, 'admin');
+create table customer (
+customerid INT NOT NULL,
+username varchar(100) NOT NULL,
+password varchar(255) NOT NULL,
+firstname varchar(100),
+lastname varchar(100),
+email varchar(100) NOT NULL,
+address varchar(255),
+phone varchar(100),
+carid int,
+primary key(customerid),
+foreign key(carid) references car(carid)
+);
 
-insert into users values
-(1, 'JamesS','James','Smith', md5(111), 'james@carrental.com',1),
-(2, 'MichaelW','Michael', 'Williams', md5(222), 'michael@carrental.com',2),
-(3, 'DoraW','Dora','Wang',md5(333), 'dora@carrental.com',3);
+insert into user values
+(1, 'james','James','Smith', 111, 'james@carrental.com','customer'),
+(2, 'michael','Michael', 'Williams', 222, 'michael@carrental.com','staff'),
+(3, 'dora','Dora','Wang',333, 'dora@carrental.com','admin');
 
-insert into cars values
-(1, 'ABC111', 'Toyota Prius', 1, 'rent out', 50.05),
-(2, 'ABC222', 'Ford Focus', NUll, 'in garage', 68.88),
-(3, 'ABC333', 'Mazda CX-60', NULL, 'in garage', 88.88);
+insert into car values
+(1, 'ABC111', 'Toyota Prius','sliver','2021', 'rent out', 50.05),
+(2, 'ABC222', 'Ford Focus','red', '2023','in garage', 68.88),
+(3, 'ABC333', 'Mazda CX-60','black','2022','in garage',88.88);
 
 
 
