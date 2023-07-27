490b2e901b612655f4440acbbba881fa12f4f172
dorachenxiW <Dora.Wang@lincolnuni.ac.nz>
2023-07-27 14:49:49 +1200

    initial changes

--- app.py
+++ app.py
@@ -28,3 +28,86 @@ def getCursor():
 @app.route("/")
 def home():
     return render_template("home1.html")
+
+# http://localhost:5000/login/ - this will be the login page, we need to use both GET and POST requests
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form.get('username')
+        user_password = request.form.get('password')
+        # Check if account exists using MySQL
+        connection = getCursor()
+        connection.execute('SELECT * FROM users WHERE username = %s', (username,))
+        # Fetch one record and return result
+        account = connection.fetchone()
+        print(account)
+        if account is not None:
+            password = account[4]
+            if bcrypt.checkpw(user_password.encode('utf-8'),password.encode('utf-8')):
+            # If account exists in accounts table in out database
+            # Create session data, we can access this data in other routes
+                session['loggedin'] = True
+                session['id'] = account[0]
+                session['username'] = account[1]
+                # Redirect to home page
+                return redirect(url_for('home'))
+            else:
+                #password incorrect
+                msg = 'Incorrect password!'
+        else:
+            # Account doesnt exist or username incorrect
+            msg = 'Incorrect username'
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
+
+""" # http://localhost:5000/home - this will be the home page, only accessible for loggedin users
+@app.route('/home')
+def home():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+ """
+
+# http://localhost:5000/register - this will be the registration page, we need to use both GET and POST requests
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            print(hashed)
+            cursor.execute('INSERT INTO users VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
