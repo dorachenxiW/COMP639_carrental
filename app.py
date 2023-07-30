69fd8bb613759ad252a940d1584a746cddd4cf11
dorachenxiW <Dora.Wang@lincolnuni.ac.nz>
2023-07-30 20:29:33 +1200

    30 July

--- app.py
+++ app.py
@@ -30,12 +30,12 @@ def getCursor():
 #http://localhost:5000/ - this is the default page. From this page, user can choose to log in or register. Admin button will be directed to the login page as well.  
 @app.route("/")
 def default():
-    return render_template("default.html")
+    return render_template('default.html')
 
 #http://locoalhost:5000/admin/ -- this is the admin page.
 @app.route("/admin/")
 def admin():
-    return render_template("admin.html")
+    return render_template('admin.html')
 
 # http://localhost:5000/login/ - this will be the login page, we need to use both GET and POST requests
 @app.route('/login/', methods=['GET', 'POST'])
@@ -52,7 +52,6 @@ def login():
         connection.execute('SELECT * FROM user WHERE username = %s', (username,))
         # Fetch one record and return result
         account = connection.fetchone()
-        print(account)
         if account is not None:
             password = account[2]
             if bcrypt.checkpw(user_password.encode('utf-8'),password.encode('utf-8')):
@@ -61,6 +60,9 @@ def login():
                 session['loggedin'] = True
                 session['id'] = account[0]
                 session['username'] = account[1]
+                #redirect to admin page if role == "admin"
+                if account[4] == 'admin':
+                    return redirect(url_for('admin'))
                 # Redirect to home page
                 return redirect(url_for('home'))
             else:
@@ -84,14 +86,34 @@ def logout():
 
 
  # http://localhost:5000/home - this will be the home page, only accessible for loggedin users
-@app.route('/home')
+@app.route('/home', methods=['GET'])
 def home():
     # Check if user is loggedin
     if 'loggedin' in session:
         # User is loggedin show them the home page
-        return render_template('home.html', username=session['username'])
+        #connect to db
+        connection = getCursor()
+        # Get car information from db
+        sql_car = 'select * from car;'
+        connection.execute(sql_car)
+        car = connection.fetchall()
+
+        return render_template('home.html', username=session['username'], car = car)
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
+
+#http://localhost:5000/home/cardetails
+@app.route('/home/cardetails', methods=['GET','POST'])
+def cardetails():
+    if 'loggedin' in session:
+        numberplate = request.form.get('numberplate')
+        print(numberplate)
+        connection=getCursor()
+        sql_indivcar='select * from car where numberplate = %s;'
+        connection.execute(sql_indivcar, (numberplate,))
+        indivcar=connection.fetchone()
+        print(indivcar)
+        return render_template('cardetails.html', username=session['username'], item=indivcar)
 
 # http://localhost:5000/register - this will be the registration page, we need to use both GET and POST requests
 @app.route('/register', methods=['GET', 'POST'])
@@ -120,9 +142,14 @@ def register():
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
             hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            print(hashed)
-            connection.execute('INSERT INTO user (username, password, email) VALUES (%s, %s, %s);', (username, hashed, email),)
-            #mysql.connection.commit()
+            #print(hashed)
+            connection.execute('INSERT INTO user (username, password, email, role) VALUES (%s, %s, %s, %s);', (username, hashed, email, 'customer'),)
+            # connect to db to get userid out from the user table after the new user has been added
+            connection.execute('Select userid from user WHERE username=%s;', (username,))
+            userid = connection.fetchone()
+            #print(userid)
+            # insert the new user information into the customer table as well, only userid will be recorded in this table when a customer registers
+            connection.execute('INSERT INTO customer (userid) VALUES (%s);', (userid[0],))
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
         # Form is empty... (no POST data)
@@ -137,9 +164,45 @@ def profile():
     if 'loggedin' in session:
         # We need all the account info for the user so we can display it on the profile page
         connection = getCursor()
-        connection.execute('SELECT * FROM user WHERE userid = %s', (session['id'],))
+        #Get the info from the table user join table customer
+        sql='SELECT * FROM user LEFT JOIN customer ON user.userid = customer.userid WHERE user.userid = %s;'
+        connection.execute(sql, (session['id'],))
         account = connection.fetchone()
         # Show the profile page with account info
         return render_template('profile.html', account=account)
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
+
+
+@app.route('/profile/update', methods=['GET','POST'])
+def updateinfo():
+    if 'loggedin' in session:
+        connection = getCursor()
+        #Get the info from the table user join table customer
+        sql='SELECT * FROM user LEFT JOIN customer ON user.userid = customer.userid WHERE user.userid = %s;'
+        connection.execute(sql, (session['id'],))
+        account = connection.fetchone()
+        return render_template('profile_update.html', account=account)
+    return redirect(url_for('login'))
+
+@app.route('/profile/edit', methods=['GET','POST'])
+def editinfo():
+    if 'loggedin' in session:
+        # Get the updated info from the form
+        
+        username=request.form.get('username')
+        #print(username)
+        firstname=request.form.get('firstname')
+        lastname=request.form.get('lastname')
+        address=request.form.get('address')
+        phone=request.form.get('phone')
+        password=request.form.get('password')
+        email=request.form.get('email')
+        hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        connection = getCursor()
+        connection.execute('UPDATE user SET username=%s, password=%s, email=%s WHERE userid=%s;', (username, hashed, email, session['id'],))
+        connection.execute('UPDATE customer SET userid=%s, firstname=%s, lastname=%s, address=%s, phone=%s;', (session['id'], firstname, lastname, address, phone,))
+        return redirect(url_for('profile'))
+    return redirect(url_for('login'))
+    
+    
